bb7197cd8d2c3c4b9604b52c8d132ba74da4ca9b
Greadmeat <greedmeat019@gmail.com>
2025-03-19 23:18:01 +0000

    rubokop

--- spec/factories/clothings.rb
+++ spec/factories/clothings.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :clothings do
-    classification {"t-shirts"}
-    brand {"underarmour"}
-    name {"Polo UA Matchplay"}
-    price {40}
-    material {"94 % polyester / 6 % élasthanne"}
-    size {"XL,M,L"}
-    for_whom {"for_men"}
+    classification { "t-shirts" }
+    brand { "underarmour" }
+    name { "Polo UA Matchplay" }
+    price { 40 }
+    material { "94 % polyester / 6 % élasthanne" }
+    size { "XL,M,L" }
+    for_whom { "for_men" }
   end
 end
