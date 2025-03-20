e3bf43ef8f0b342ab226b6930bf8efe8b0c69ff3
Greadmeat <greedmeat019@gmail.com>
2025-03-20 23:31:37 +0000

    for hum for all categories + sidebar decor

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -7,23 +7,38 @@ class ClothingsController < ApplicationController
   end
   def t_shirts
     @t_shirts = Clothings.where(classification: "t-shirts")
+    if params[:for_whom].present?
+      @t_shirts = @t_shirts.where(for_whom: params[:for_whom])
+    end
   end
   def shoes
     @shoes = Clothings.where(classification: "shoes")
     if params[:for_whom].present?
-      @shoes = @shoes.where(for_whom: params[:for_whom]) # Фильтруем по category, если он передан
+      @shoes = @shoes.where(for_whom: params[:for_whom])
     end
   end
   def jackets
     @jackets = Clothings.where(classification: "jackets")
+    if params[:for_whom].present?
+      @jackets = @jackets.where(for_whom: params[:for_whom])
+    end
   end
   def sweaters
     @sweaters = Clothings.where(classification: "sweaters")
+    if params[:for_whom].present?
+      @sweaters = @sweaters.where(for_whom: params[:for_whom])
+    end
   end
   def business_suits
     @business_suits = Clothings.where(classification: "business_suits")
+    if params[:for_whom].present?
+      @business_suits = @business_suits.where(for_whom: params[:for_whom])
+    end
   end
   def headwear
     @headwear = Clothings.where(classification: "headwear")
+    if params[:for_whom].present?
+      @headwear = @headwear.where(for_whom: params[:for_whom])
+    end
   end
 end
