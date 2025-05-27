9e38c815858801c0e219b57f52ef3d9056579d17
Pavel Herasimau <pavelgnom@gmail.com>
2025-05-27 09:58:28 +0200

    Ignore auth0 credentials for test env

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,19 +1,21 @@
-OmniAuth.config.allowed_request_methods = [ :post, :get ]
-OmniAuth.config.silence_get_warning = true
+if Rails.env.development? or Rails.env.production?
+  OmniAuth.config.allowed_request_methods = [ :post, :get ]
+  OmniAuth.config.silence_get_warning = true
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  auth0_config = Rails.application.credentials.dig(:auth0)
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    auth0_config = Rails.application.credentials.dig(:auth0)
 
-  raise "Missing Auth0 credentials!" if auth0_config.blank?
+    raise "Missing Auth0 credentials!" if auth0_config.blank?
 
-  provider(
-    :auth0,
-    auth0_config[:client_id],
-    auth0_config[:client_secret],
-    auth0_config[:domain],
-    callback_path: "/auth/auth0/callback",
-    authorize_params: {
-      scope: "openid profile"
-    }
-  )
+    provider(
+      :auth0,
+      auth0_config[:client_id],
+      auth0_config[:client_secret],
+      auth0_config[:domain],
+      callback_path: "/auth/auth0/callback",
+      authorize_params: {
+        scope: "openid profile"
+      }
+    )
+  end
 end
